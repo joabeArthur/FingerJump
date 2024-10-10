830ca764e7969f5e9031cfc06a00ceac40fbb844
joabe <joalokk@gmail.com>
2024-10-10 11:50:28 -0300

    quase

--- FingerJump/MainPage.xaml.cs
+++ FingerJump/MainPage.xaml.cs
@@ -2,9 +2,39 @@
 
 public partial class MainPage : ContentPage
 {
+	const int Gravidade = 1;
+	const int TempoEntreFremes = 25;
+	bool Morte = false;
 	public MainPage()
 	{
 		InitializeComponent();
+	}
+
+	void ColocarGravidade()
+	{
+	//	cachorro.TranslationY += Gravidade;
+	}
+
+   
+	async Task Desenhar()
+	{
+		while (!Morte)
+		{
+			ColocarGravidade();
+			await Task.Delay(TempoEntreFremes);
+		}
+	}
+
+	void Cabo(object s, TappedEventArgs a)
+	{
+	//	FrameAcabou.IsVisible = false;
+		Inicializar();
+		Desenhar();
+	}
+
+	void Inicializar()
+	{
+	//	cachorro.TranslationY = 0;
 	}
 }
 
